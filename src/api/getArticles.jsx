15824aa4e12ff5f11b7dfd7aca2e99c70968354c
Marco Rehberg <marcorehberg@gmx.net>
2025-09-30 14:50:51 +0200

    Comment

--- src/api/getArticles.jsx
+++ src/api/getArticles.jsx
@@ -6,7 +6,6 @@ export const getArticles = async (url, signal) => {
     if (!res.ok) {
       throw new Error("Api läuft nicht");
     }
-
     return await res.json();
   } catch (error) {
     // Fehler bewusst nach außen weitergeben
